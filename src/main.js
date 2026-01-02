651bb6638a4ffe53820866db3657965f604bc607
Kenji <kenjisakai80@gmail.com>
2026-01-02 15:02:49 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,4 @@
+"use strict";
+
+// CSS
+import "./styles/global.css";
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "main.js"),
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+      favicon: path.resolve(__dirname, "src", "assets", "favicon.svg"),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets"),
+          to: path.resolve(__dirname, "dist", "src", "assets"),
+        },
+      ],
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            targets: "defaults",
+            presets: [["@babel/preset-env"]],
+          },
+        },
+      },
+    ],
+  },
+
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist", "index.html"),
+    },
+    open: true,
+    port: 3000,
+  },
+};
