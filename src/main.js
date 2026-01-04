9a926b9bf83040979d26dda95e1f09b7701507c1
Kenji <kenjisakai80@gmail.com>
2026-01-04 10:37:41 -0300

    ajust final

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@
 
 // CSS
 import "./styles/global.css";
+import "./styles/utility.css";
+import "./styles/font.css";
 import "./styles/schedule.css";
 import "./styles/modal.css";
+import "./styles/form.css";
